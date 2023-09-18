c41c22c88476ff471c72759b5689740820fffdc8
Elina Karapetian <elina.karapetyan.00@mail.ru>
2023-09-18 16:23:59 +0400

    note controller

---
+++ lesson3.5_homework/Assembles/NoteViewControllerAssembly.swift
@@ -0,0 +1,8 @@
+//
+//  NoteViewControllerAssembly.swift
+//  lesson3.5_homework
+//
+//  Created by Элина Карапетян on 18.09.2023.
+//
+
+import Foundation
---
+++ lesson3.5_homework/Views/NoteView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteView.swift
+//  lesson3.5_homework
+//
+//  Created by Элина Карапетян on 18.09.2023.
+//
+
+import UIKit
+
+class NoteView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
